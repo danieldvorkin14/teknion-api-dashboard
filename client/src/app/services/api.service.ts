2992539f1f4d08c85ff3d5063d0f06f41903c8b8
Daniel Dvorkin <daniel.dvorkin@teknion.com>
2020-04-06 15:25:07 -0400

    setup the api service to detect what endpoint to hit

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BindingFlags } from '@angular/compiler/src/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  API_URL: string = "http://localhost:3000/";
-
   constructor(public http: HttpClient) { }
 
+  get_host(){
+    if(window.location.hostname == "localhost"){
+      return "http://localhost:3000/";
+    } else {
+      return window.location.hostname + "/";
+    }
+  }
+
   get(path) {
-    var endpoint = this.API_URL + path;
+    var endpoint = this.get_host() + path;
     
     return this.http.get(endpoint);
   }
 
   post(path, body){
-    var endpoint = this.API_URL + path;
+    var endpoint = this.get_host() + path;
 
     return this.http.post(endpoint, body);
   }
 
   delete(path){
-    var endpoint = this.API_URL + path;
+    var endpoint = this.get_host() + path;
 
     return this.http.delete(endpoint);
   }
 
   update(path, body){
-    var endpoint = this.API_URL + path;
+    var endpoint = this.get_host() + path;
 
     return this.http.put(endpoint, body);
   }
